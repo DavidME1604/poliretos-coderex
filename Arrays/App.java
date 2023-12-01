1b73357268e8ecfe37f325cc707f38dfd48dd4ed
DavidME1604 <davidestebanmoralese@gmail.com>
2023-11-30 22:09:13 -0500

    estadarizacion prt2

--- Arrays/App.java
+++ Arrays/App.java
@@ -2,8 +2,10 @@ package Arrays;
 
 public class App {
     public static void main(String[] args) {
-        Arrays4NombreMatriz array4 = new Arrays4NombreMatriz();
-        array4.nombreMatriz();
+        Arrays4NombreMatriz oArray4 = new Arrays4NombreMatriz();
+        System.out.println("Nombre en una matriz en X: ");
+        oArray4.nombreMatriz();
+        System.out.println();
     }
 
 }
--- CadenaCaracteres/App.java
+++ CadenaCaracteres/App.java
@@ -2,10 +2,14 @@ package CadenaCaracteres;
 
 public class App {
     public static void main(String[] args) {
-        Cadena3EliminarVocal cadena3 = new Cadena3EliminarVocal();
-        cadena3.EliminarVocal();
+        Cadena3EliminarVocal oCadena3 = new Cadena3EliminarVocal();
+        System.out.println("Elimina vocales de una palabra: ");
+        oCadena3.EliminarVocal();
         System.out.println();
-        Cadena8Anagramas cadena8 = new Cadena8Anagramas();
-        cadena8.Anagramas();
+
+        System.out.println("\nJuego de los anagramas:");
+        Cadena8Anagramas oCadena8 = new Cadena8Anagramas();
+        oCadena8.Anagramas();
+        System.out.println();
     }
 }
