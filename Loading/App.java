c087ae3de8446cceab8a52e17283bcabbe408929
Vladimirjon <johannpasquel@gmail.com>
2023-12-03 11:52:31 -0500

    Invocar a los metodos JP

--- Loading/App.java
+++ Loading/App.java
@@ -6,10 +6,20 @@ public class App {
     public static void main(String[] args) {
         Scanner reader = new Scanner(System.in);
 
+        Loading2hashtag oLoading2 = new Loading2hashtag();
+        System.out.println("\nBarra de carga con (#): ");
+        oLoading2.hashtagload();
+        System.out.println();
+
 
         Loading4Bolitas oLoading4 = new Loading4Bolitas();
         System.out.println("Cargando bolitas (oo0)...");
         oLoading4.bolitas();
+        System.out.println();
+
+        Loading7flecha oLoading7 = new Loading7flecha();
+        System.out.println("\nBarra de carga con flecha: ");
+        oLoading7.barraflecha();
         System.out.println();
 
 
