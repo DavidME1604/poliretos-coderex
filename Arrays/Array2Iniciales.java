81016a4f90e1e709752989235e1e9de736991940
Vladimirjon <johannpasquel@gmail.com>
2023-12-03 10:04:51 -0500

    array

--- Arrays/Array2Iniciales.java
+++ Arrays/Array2Iniciales.java
@@ -1,56 +1,61 @@
 /*
- * Crear una matriz solicitando el tamaño y caracter para almacenar la iniciales de su nombre y apellido y presentar la matriz
-            Ejemplo:    Mi nombre es Johann Pasquel => JP    
-            Ingrese el tamaño: 5
-            Ingrese caracter : *
-
-            *****   *****      
-              *     *   *      
-              *     *****      
-              *     *                   
-            ***     *  
- */
+    * @author Johann Vladimir Pasquel Montenegro
+    * @Github Vladimirjon
+    * version 1.0  
+    *
+    * @since 2023-12-01
+    * 
+    * @license
+    * Crear una matriz solicitando el tamaño y caracter para almacenar la iniciales de su nombre y apellido y presentar la matriz
+    *        Ejemplo:    Mi nombre es Johann Pasquel => JP    
+    *        Ingrese el tamaño: 5
+    *        Ingrese caracter : *
+    *
+    *        *****   *****      
+    *          *     *   *      
+    *          *     *****      
+    *          *     *                   
+    *        ***     *  
+*/
 
 package Arrays;
 
 public class Array2Iniciales {
+    /**
+     * Se trabajara con la creacion de las dos letras para mas tarde unirlas
+     * @param tamaño
+     * @param caracter
+     */
     public void incialesletras(int tamaño,char caracter){
-        char[][] letraJ = new char[tamaño][tamaño];
-        char[][] letraP = new char[tamaño][tamaño];
+        char[][] primera = new char[tamaño][tamaño];
+        char[][] segunda = new char[tamaño][tamaño];
 
-        // fill matrices with initials
+        for (int i = 0; i < primera.length; i++) {
+            for (int j = 0; j < primera[i].length; j++) {
+                if ((i == 0) || (j == tamaño / 2) || (j < tamaño / 2 && i == tamaño - 1)) {
+                    primera[i][j] = caracter;
+                } else {
+                    primera[i][j] = ' ';
+                }
+                if ((i == 0 || i == tamaño / 2 || j == 0) || (i < tamaño / 2 && j == segunda[i].length - 1) || (j > tamaño / 2 && j == 0)) {
+                    segunda[i][j] = caracter;
+                } else {
+                    segunda[i][j] = ' ';
+                }
+            }
+        }
+        char[][] matriz = new char[tamaño][2 * tamaño + 2]; 
+
         for (int i = 0; i < tamaño; i++) {
-            letraJ[i][0] = caracter;
-            letraJ[i][tamaño - 1] = caracter;
-            letraJ[tamaño - 1][i] = caracter;
-            if (i >= tamaño / 2) {
-                letraJ[i][tamaño - 1] = caracter;
-            }
-            letraP[i][0] = caracter;
-            letraP[0][i] = caracter;
-            letraP[i][tamaño - 1] = caracter;
-            if (i == 0 || i == tamaño / 2) {
-                letraP[i][1] = caracter;
-            }
-            if (i == 1 || i == tamaño / 2) {
-                letraP[i][2] = caracter;
-            }
-            if (i == 2 || i == tamaño / 2) {
-                letraP[i][3] = caracter;
-            }
-            if (i == 3 || i == tamaño / 2) {
-                letraP[i][4] = caracter;
+            for (int j = 0; j < tamaño; j++) {
+                matriz[i][j] = primera[i][j];
+                matriz[i][j + tamaño + 2] = segunda[i][j];
             }
         }
 
-        //imprimir matrices 
-        for (int i = 0; i < tamaño; i++) {
-            for (int j = 0; j < 2 * tamaño - 1; j++) {
-                System.out.print(letraJ[i][j]);
-            }
-            System.out.print("  ");
-            for (int j = 0; j < 2 * tamaño - 1; j++) {
-                System.out.print(letraP[i][j]);
+        for (int i = 0; i < tamaño + 1; i++) {
+            for (int j = 0; j < 2 * tamaño + 2; j++) {
+                System.out.print(matriz[i][j]);
             }
             System.out.println();
         }
