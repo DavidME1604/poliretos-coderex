8881c43b0913daf0ffbf75f27468a3589bc7dab5
Lorena <lopezzelayalore@gmail.com>
2023-12-03 19:11:49 -0500

    doc. Cadena 7

--- CadenaCaracteres/Cadena7EliminarJ.java
+++ CadenaCaracteres/Cadena7EliminarJ.java
@@ -1,14 +1,22 @@
 package CadenaCaracteres;
-
 import java.util.Scanner;
+/**
+     * @author Elizabeth Lopez Zelaya
+     * @Github Lorent777
+     * @version 2.0
+     * 
+     * @since 2023-12-01
+     * 
+     * @license
+     * 
+     *         
+     */
 
 public class Cadena7EliminarJ {
+
     public void  eliminarJota(String palabraJ) {
 
-        // Convertir la palabra a mayúsculas y eliminar la letra 'J'
         String resultado = palabraJ.toUpperCase().replace("J", "");
-
-        // Mostrar el resultado
         System.out.println("La palabra sin la letra 'J' es: " + resultado);
 
     }
