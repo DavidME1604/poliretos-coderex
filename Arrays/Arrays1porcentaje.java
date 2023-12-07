72e84d9bf54976eaa5379473eb46b8f7352f5e43
Jonathanparedes4325 <133798156+Jonathanparedes4325@users.noreply.github.com>
2023-12-06 21:08:56 -0500

    Cambio

--- Arrays/Arrays1porcentaje.java
+++ Arrays/Arrays1porcentaje.java
@@ -1,57 +1,44 @@
 package Arrays;
 import java.util.Scanner;
-
 /**
  * Clase que permite ingresar un nombre completo, solicitar porcentajes de carga
- * para cada palabra y mostrar una representación del porcentaje de carga
- * para cada palabra.
- * 
+ * para cada palabra 
  * @author Jonathan Paredes
  * @version 2.43
  * @since 2023-12-03
  */
-public class Arrays1porcentaje {
-
+public class Arrays1CargaPorcentajePalabra {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
         
-        System.out.print("Ingrese su nombre completo: ");
-        String nombreCompleto = scanner.nextLine();
+        System.out.println("Ingrese su nombre: ");
+        String nombre = scanner.nextLine();
+
+       
+        String[] palabras = nombre.split(" ");
 
         
-        String[] palabras = nombreCompleto.split("\\s+");
+        System.out.println("Ingrese el porcentaje de carga : ");
+        String[] porcentajesStr = scanner.nextLine().split(" ");
+        int[] porcentajes = new int[palabras.length];
 
         
-        int[] porcentajesCarga = new int[palabras.length];
-
-        
-        for (int i = 0; i < palabras.length; i++) {
-            System.out.print("Ingrese el porcentaje de carga para \"" + palabras[i] + "\": ");
-            porcentajesCarga[i] = scanner.nextInt();
+        for (int i = 0; i < porcentajesStr.length; i++) {
+            porcentajes[i] = Integer.parseInt(porcentajesStr[i]);
         }
 
-        
-        System.out.println("\n" + nombreCompleto);
-
         for (int i = 0; i < palabras.length; i++) {
-            mostrarPorcentajeCarga(palabras[i], porcentajesCarga[i]);
+            mostrarBarra(palabras[i], porcentajes[i]);
         }
 
         scanner.close();
     }
 
-    private static void mostrarPorcentajeCarga(String palabra, int porcentaje) {
-        int longitudBarra = porcentaje / 5;
-        int espacios = 20 - longitudBarra;
+    private static void mostrarBarra(String palabra, int porcentaje) {
+        int largoBarra = porcentaje / 5;
+        int espacios = 20 - largoBarra;
 
-        System.out.print("[");
-        for (int i = 0; i < longitudBarra; i++) {
-            System.out.print("=");
-        }
-        for (int i = 0; i < espacios; i++) {
-            System.out.print(" ");
-        }
-        System.out.println("] " + porcentaje + "% " + palabra);
+        System.out.println("[" + "=".repeat(largoBarra) + ">" + " ".repeat(espacios) + "]" + porcentaje + "% " + palabra);
     }
 }
