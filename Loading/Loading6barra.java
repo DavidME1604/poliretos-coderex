3d0296e2fa7cd581b548cf73ac91a2270b8a6c23
Jonathanparedes4325 <133798156+Jonathanparedes4325@users.noreply.github.com>
2023-12-08 12:08:14 -0500

    Loading6barra.

--- Loading/Loading6barra.java
+++ Loading/Loading6barra.java
@@ -10,13 +10,30 @@ package Loading;
  * @license Loading, Usar el delay para animar
  */
 public class Loading6Barra {
-    
+    public void iniciarBarraDeslizante() {
+        int retardo = 2000;
+        int pasos = 20;
 
-    public static void mostrarBarraDesplazamiento(int porcentaje) {
+        
+        for (int i = 0; i <= pasos; i++) {
+            int porcentaje = i * 100 / pasos;
+            mostrarBarraDesplazamiento(porcentaje);
+            pausar(retardo / pasos);
+        }
+
+        
+        for (int i = pasos; i >= 0; i--) {
+            int porcentaje = i * 100 / pasos;
+            mostrarBarraDesplazamiento(porcentaje);
+            pausar(retardo / pasos);
+        }
+    }
+
+    private static void mostrarBarraDesplazamiento(int porcentaje) {
         int longitudBarra = 20;
         int posicionFlecha = (int) Math.ceil((porcentaje / 100.0) * longitudBarra);
 
-        //Construir la barra de desplazamiento
+        
         StringBuilder barra = new StringBuilder("[");
         for (int i = 0; i < longitudBarra; i++) {
             if (i == posicionFlecha) {
@@ -27,11 +44,11 @@ public class Loading6Barra {
         }
         barra.append("] ").append(porcentaje).append("%");
 
-        //Barra animada
+        
         System.out.print("\r" + barra.toString());
     }
 
-    public static void pausar(int milisegundos) {
+    private static void pausar(int milisegundos) {
         try {
             Thread.sleep(milisegundos);
         } catch (InterruptedException e) {
