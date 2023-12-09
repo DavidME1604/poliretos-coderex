32409d0f9cefcee8be4be960a94f4da4597c95e0
Lorena <lopezzelayalore@gmail.com>
2023-12-09 18:11:16 -0500

    Mod. App.loading - Loadin 8 / Corr. Bug

--- Loading/Loading8NombreApellido.java
+++ Loading/Loading8NombreApellido.java
@@ -3,26 +3,29 @@ package Loading;
      * @author Elizabeth Lopez Zelaya
      * @Github Lorent777
      * @version 2.0
-     * 
      * @since 2023-12-01
-     * 
-     * @license
-     * 
-     *         
+     * @license         
      */
 
 public class Loading8NombreApellido {
+    
+    public void loading8NombreApellido(String nombreCompleto) {
+        int longitudBarra = 20;
 
-    public void loading8Nombre(String nombreCompleto) {
-        char[] nombreArray = nombreCompleto.toCharArray();
-        System.out.print("[");
-        
-        for (char c : nombreArray) {
-            System.out.print(c);
+        for (int i = 0; i <= nombreCompleto.length(); i++) {
+            String nombreCargando = "[";
+
+            for (int j = 0; j < nombreCompleto.length(); j++) {
+                nombreCargando += (j < i) ? nombreCompleto.charAt(j) : " ";
+            }
+
+            nombreCargando += "]";
+            
+            int porcentaje = i * 100 / nombreCompleto.length();
+            System.out.print("\r" + nombreCargando + " " + porcentaje + "%");
+
             esperar(200);
         }
-        
-        System.out.println("] 100%");
     }
 
     private static void esperar(int milisegundos) {
@@ -31,6 +34,6 @@ public class Loading8NombreApellido {
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
-    }
+    }   
 }
 
