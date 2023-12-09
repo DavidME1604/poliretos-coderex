a51f14aff5ab2bd5fb450146e27935240391c05a
Jonathanparedes4325 <133798156+Jonathanparedes4325@users.noreply.github.com>
2023-12-09 16:37:33 -0500

    cambio

--- Loading/Loading6Barra.java
+++ Loading/Loading6Barra.java
@@ -14,26 +14,21 @@ public class Loading6Barra {
         int retardo = 2000;
         int pasos = 20;
 
-        
         for (int i = 0; i <= pasos; i++) {
-            int porcentaje = i * 100 / pasos;
-            mostrarBarraDesplazamiento(porcentaje);
+            mostrarBarraDesplazamiento(i, pasos, i == pasos);
             pausar(retardo / pasos);
         }
 
-        
         for (int i = pasos; i >= 0; i--) {
-            int porcentaje = i * 100 / pasos;
-            mostrarBarraDesplazamiento(porcentaje);
+            mostrarBarraDesplazamiento(i, pasos, i == 0);
             pausar(retardo / pasos);
         }
     }
 
-    private static void mostrarBarraDesplazamiento(int porcentaje) {
+    private static void mostrarBarraDesplazamiento(int progreso, int total, boolean mostrarPorcentaje) {
         int longitudBarra = 20;
-        int posicionFlecha = (int) Math.ceil((porcentaje / 100.0) * longitudBarra);
+        int posicionFlecha = (int) Math.ceil((progreso / (double) total) * longitudBarra);
 
-        
         StringBuilder barra = new StringBuilder("[");
         for (int i = 0; i < longitudBarra; i++) {
             if (i == posicionFlecha) {
@@ -42,9 +37,12 @@ public class Loading6Barra {
                 barra.append(" ");
             }
         }
-        barra.append("] ").append(porcentaje).append("%");
+        barra.append("] ");
 
-        
+        if (mostrarPorcentaje) {
+            barra.append("100%");
+        }
+
         System.out.print("\r" + barra.toString());
     }
 
